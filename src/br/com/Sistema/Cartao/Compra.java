c4d021ca9f2f541a9dff7d34ec9ee035b8691e59
okauezo <kauepiresmoreira09@gmail.com>
2026-02-16 15:28:29 -0300

    Classe de Compra criada e passando atributos a ela

--- src/br/com/Sistema/Cartao/Compra.java
+++ src/br/com/Sistema/Cartao/Compra.java
@@ -2,4 +2,26 @@ package br.com.Sistema.Cartao;
 
 public class Compra {
 
+    private String descricao;
+    private double valor;
+
+    public Compra(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor =  valor;
+    }
+
+    public String getDescricao() {        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 }
